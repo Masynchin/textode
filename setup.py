0b5e49e289091fc4d8fb11a3659223262bdec20b
Masynchin <masynchin@gmail.com>
2022-01-23 18:02:25 +0300

    bumb version 1.3.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="textode",
-    version="1.2.0",
+    version="1.3.0",
     description="Make your text-bot with only one handler",
     url="https://guthib.com/Masynchin/textode",
     author_email="masynchin@gmail.com",
