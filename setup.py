60b64f26747723a7d9081825cfa35a74b7876da2
Masynchin <masynchin@gmail.com>
2022-01-24 21:58:33 +0300

    fix typos in setup.py and add long description

--- setup.py
+++ setup.py
@@ -1,13 +1,24 @@
+from pathlib import Path
 from setuptools import find_packages, setup
+
+
+CURRENT_DIR = Path(__file__).parent
+
+
+def get_long_description() -> str:
+    """Get long description from README.md."""
+    return (CURRENT_DIR / "README.md").read_text(encoding="u8")
 
 
 setup(
     name="textode",
     version="1.3.0",
-    description="Make your text-bot with only one handler",
-    url="https://guthib.com/Masynchin/textode",
+    description="Make your text-bot with only one handler.",
+    long_description=get_long_description(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Masynchin/textode",
     author_email="masynchin@gmail.com",
     license="MIT",
     packages=find_packages(include=["textode"]),
-    python_requires=">=3.7",  # as asyncio does
+    python_requires=">=3.7",
 )
